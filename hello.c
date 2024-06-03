c20ea7ca7c21ded452007751f492979dcd426260
NIVEDITHA-2 <nivedithavava1@gmail.com>
2024-06-03 11:58:52 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+
+int main(){
+    
+
+    // dataypes-int,char,float,double,string
+    // int choice;
+// printf("enter your name:");
+// scanf("%d",&choice);
+int a= 5;
+char s='e';
+float b=4.5;
+double d=4.566666666;
+char ee[]="hello";
+printf("%s",ee);
+printf("%d",a);
+printf("\n%c",s);
+printf("\n%f",b);
+printf("\n%lf",d);
+return 0;
+}
+
